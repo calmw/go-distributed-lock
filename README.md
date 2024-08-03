68c7f2729ec503fbc9687fd08fa875b6396306ab
calmw <calm.wang@hotmail.com>
2024-08-03 16:03:14 +0800

    添加example

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # go-distributed-lock
 
 - golang 实现的基于GRPC的分布式锁
-- 服务端：https://github.com/calmw/go-distributed-lock-server
 
 ## 使用方法
 
-```go
+- 1 部署服务端：https://github.com/calmw/go-distributed-lock-server
 
-```
+- 2 客户端使用
+    - demo [dislock.go](example%2Fdislock.go)
---
+++ example/dislock.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	dislock "github.com/calmw/go-distributed-lock"
+	"log"
+	"os"
+	"time"
+)
+
+const (
+	DefaultServerAddr = "127.0.0.1:6000"
+)
+
+var Lk *dislock.Lock
+
+func main() {
+	serverAddr := os.Getenv("SERVER_ADDR")
+	if len(serverAddr) == 0 {
+		serverAddr = DefaultServerAddr
+	}
+	Lk = dislock.NewLock(serverAddr, 200, 3000)
+
+	TaskLock("order_lock", "userA")
+	log.Println("订单操作")
+	time.Sleep(time.Second * 10)
+	TaskUnLock("order_lock", "userA")
+}
+
+func TaskLock(lockName, clientId string) {
+	t := time.Now()
+	log.Printf("%s %s,加锁...", lockName, clientId)
+	_, _ = Lk.Lock(lockName, clientId)
+
+	log.Printf("%s %s %d, 执行加锁后的业务...", lockName, clientId, time.Since(t))
+}
+
+func TaskUnLock(lockName, clientId string) {
+	log.Printf("%s %s,释放锁...", lockName, clientId)
+	t := time.Now()
+	_, _ = Lk.UnLock(lockName, clientId)
+
+	log.Printf("%s %s %d,执行释放锁后的业务...", lockName, clientId, time.Since(t))
+}
